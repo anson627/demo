010d5804c20c31d0d9a25827b55511ef0c81af05
Anson Qian <anson627@gmail.com>
2024-10-10 15:06:57 -0400

    update

--- gpu-test/cleanup.sh
+++ gpu-test/cleanup.sh
@@ -3,9 +3,7 @@
 set -eo pipefail
 
 SUBSCRIPTION=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
-LOCATION=eastus
 RESOURCE_GROUP=gpu-test
-CLUSTER_NAME=operator-test
 
 az account set -s $SUBSCRIPTION
-az group delete -n $RESOURCE_GROUP --yes --no-wait
+az group delete -n $RESOURCE_GROUP --yes
--- gpu-test/setup.sh
+++ gpu-test/setup.sh
@@ -3,12 +3,15 @@
 set -eo pipefail
 
 SUBSCRIPTION=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
-LOCATION=eastus
+LOCATION=centraluseuap
 RESOURCE_GROUP=gpu-test
-CLUSTER_NAME=operator-test
-GPU_POOL_NAME=gpupool
-GPU_POOL_SIZE=2
-GPU_VM_SIZE=Standard_HB120-16rs_v3
+CLUSTER_NAME=gpu-test
+SYSTEM_POOL_NAME=system
+SYSTEM_VM_SIZE=Standard_D8s_v5
+SYSTEM_POOL_SIZE=3
+USER_POOL_NAME=gpu
+USER_POOL_SIZE=2
+USER_VM_SIZE=Standard_ND96isr_H100_v5
 
 az account set -s $SUBSCRIPTION
 if az group show -n $RESOURCE_GROUP &>/dev/null; then
@@ -25,21 +28,24 @@ else
     az aks create -l $LOCATION \
         -g $RESOURCE_GROUP \
         -n $CLUSTER_NAME \
+        --nodepool-name $SYSTEM_POOL_NAME \
+        --node-vm-size Standard_D8s_v5 \
+        --node-count 3 \
         --tier standard \
         --yes
 fi
 
-if az aks nodepool show --resource-group $RESOURCE_GROUP --cluster-name $CLUSTER_NAME --name $GPU_POOL_NAME &>/dev/null; then
-    echo "GPU pool already exists."
+if az aks nodepool show --resource-group $RESOURCE_GROUP --cluster-name $CLUSTER_NAME --name $USER_POOL_NAME &>/dev/null; then
+    echo "User pool already exists."
 else
-    echo "GPU pool does not exist. Creating ..."
+    echo "User pool does not exist. Creating ..."
     az aks nodepool add \
         --resource-group $RESOURCE_GROUP \
         --cluster-name $CLUSTER_NAME \
-        --name $GPU_POOL_NAME \
-        --node-taints nvidia.com/gpu=present:NoSchedule \
-        --node-vm-size $GPU_VM_SIZE \
-        --node-count $GPU_POOL_SIZE \
+        --name $USER_POOL_NAME \
+        --node-vm-size $USER_VM_SIZE \
+        --node-count $USER_POOL_SIZE \
+        --node-taints nvidia.com/gpu=present:NoSchedule
         --skip-gpu-driver-install
 fi
 
