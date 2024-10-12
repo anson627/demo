9870b3a94a785ca49ea4c2a8b80f472ed0f0eca3
Anson Qian <anson627@gmail.com>
2024-10-12 13:15:30 -0400

    update

--- gpu-test/cleanup.sh
+++ gpu-test/cleanup.sh
@@ -2,7 +2,12 @@
 
 set -eo pipefail
 
+# ACS Test with H100
 SUBSCRIPTION=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
+
+# Dataplane Developer with A100
+# SUBSCRIPTION=8643025a-c059-4a48-85d0-d76f51d63a74
+
 RESOURCE_GROUP=gpu-test
 
 az account set -s $SUBSCRIPTION
--- gpu-test/setup.sh
+++ gpu-test/setup.sh
@@ -2,16 +2,24 @@
 
 set -eo pipefail
 
+ACS Test with H100
 SUBSCRIPTION=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
 LOCATION=centraluseuap
+USER_VM_SIZE=Standard_ND96isr_H100_v5
+
+# Dataplane Developer with A100
+# SUBSCRIPTION=8643025a-c059-4a48-85d0-d76f51d63a74
+# LOCATION=southcentralus
+# USER_VM_SIZE=Standard_ND96amsr_A100_v4
+
+
 RESOURCE_GROUP=gpu-test
 CLUSTER_NAME=gpu-test
 SYSTEM_POOL_NAME=system
-SYSTEM_VM_SIZE=Standard_D8s_v5
+SYSTEM_VM_SIZE=Standard_D16s_v5
 SYSTEM_POOL_SIZE=3
 USER_POOL_NAME=gpu
-USER_POOL_SIZE=2
-USER_VM_SIZE=Standard_ND96isr_H100_v5
+USER_POOL_SIZE=1
 
 az account set -s $SUBSCRIPTION
 if az group show -n $RESOURCE_GROUP &>/dev/null; then
@@ -28,10 +36,10 @@ else
     az aks create -l $LOCATION \
         -g $RESOURCE_GROUP \
         -n $CLUSTER_NAME \
-        --nodepool-name $SYSTEM_POOL_NAME \
-        --node-vm-size Standard_D8s_v5 \
-        --node-count 3 \
         --tier standard \
+        --nodepool-name $SYSTEM_POOL_NAME \
+        --node-vm-size $SYSTEM_VM_SIZE \
+        --node-count $SYSTEM_POOL_SIZE \
         --yes
 fi
 
@@ -45,7 +53,7 @@ else
         --name $USER_POOL_NAME \
         --node-vm-size $USER_VM_SIZE \
         --node-count $USER_POOL_SIZE \
-        --node-taints nvidia.com/gpu=present:NoSchedule
+        --node-taints nvidia.com/gpu=present:NoSchedule \
         --skip-gpu-driver-install
 fi
 
