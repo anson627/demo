8bfdc4cfe46d6c6827a2bb07fbf399bd3431ac61
Anson Qian <anson627@gmail.com>
2025-03-07 15:41:51 -0500

    update gke

---
+++ gke-test/cleanup.sh
@@ -0,0 +1,1 @@
+gcloud container clusters delete cni-test --zone=us-west1-a
--- gke-test/setup.sh
+++ gke-test/setup.sh
@@ -1,3 +1,15 @@
 gcloud config set project phrasal-bond-380604
-gcloud container clusters create retina-test --zone us-west1-a
-gcloud container clusters get-credentials retina-test --zone us-west1-a
+
+if gcloud compute networks describe cni-test &>/dev/null; then
+    echo "Network cni-test already exists."
+else
+    gcloud compute networks create cni-test --mtu=8896
+fi
+
+if gcloud container clusters describe cni-test --zone=us-west1-a &>/dev/null; then
+    echo "Cluster cni-test already exists."
+else
+    gcloud container clusters create cni-test --zone=us-west1-a --network=cni-test
+fi
+
+gcloud container clusters get-credentials cni-test --zone us-west1-a
