6e9fe0e18c2d9dc46d4fd5cc3aea04fafffa1506
Anson Qian <anson627@gmail.com>
2025-05-20 21:14:36 +0000

    update

--- kwok-test/deploy.sh
+++ kwok-test/deploy.sh
@@ -6,8 +6,12 @@ NODE_COUNT=5000
 NAMESPACE_COUNT=150
 PODS_PER_NAMESPACE=1000
 
-# helm upgrade --install node node \
-#  --nodeCount "$NODE_COUNT"
+if helm list -q | grep "^node$" > /dev/null 2>&1; then
+  echo "node release already exists, skipping..."
+else
+  helm upgrade --install node node \
+  --set "nodeCount=$NODE_COUNT"
+fi
 
 for i in $(seq 1 $NAMESPACE_COUNT); do
   namespace="kwok-$i"
@@ -15,28 +19,11 @@ for i in $(seq 1 $NAMESPACE_COUNT); do
     kubectl create namespace "$namespace"
   fi
 
-  helm upgrade --install pod pod \
-    --namespace "$namespace" \
-    --set "podCount=$PODS_PER_NAMESPACE"
-done
-
-echo "Waiting for all fake pods to be running..."
-total_pods=$((NAMESPACE_COUNT * PODS_PER_NAMESPACE))
-echo "Expected total pods: $total_pods"
-
-while true; do
-  echo "Pod status breakdown:"
-  running_pods=0
-  kubectl get pod -l app=fake-pod -A --no-headers | awk '{print $4}' | sort | uniq -c | while read count status; do
-    echo "  $status: $count"
-    if [ "$status" = "Running" ]; then
-      running_pods=$((running_pods + count))
-    fi
-  done
-
-  echo "  Running pods: $running_pods / $total_pods"
-  done
-
-  echo "Waiting for remaining pods to start..."
-  sleep 10
+  if helm list -q -n "$namespace" | grep "^pod$" > /dev/null 2>&1; then
+    echo " pod release already exists in $namespace, skipping..."
+  else
+    helm upgrade --install pod pod \
+      --namespace "$namespace" \
+      --set "podCount=$PODS_PER_NAMESPACE"
+  fi
 done
