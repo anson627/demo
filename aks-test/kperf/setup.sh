8f3197ab965e15285b2d83566e92fe5c846ba0ef
Anson Qian <anson627@gmail.com>
2025-06-02 12:18:28 -0400

    update

--- aks-test/kperf/setup.sh
+++ aks-test/kperf/setup.sh
@@ -22,30 +22,27 @@ else
         --tier standard \
         --kubernetes-version 1.33.0 \
         --network-plugin azure \
+        --network-plugin-mode overlay \
         --nodepool-name system \
         --node-vm-size ${SYSTEM_VM_SIZE} \
         --node-count ${SYSTEM_POOL_SIZE} \
-        --enable-apiserver-vnet-integration \
         --custom-configuration custom-config.json \
         --aks-custom-headers OverrideControlplaneResources=W3siY29udGFpbmVyTmFtZSI6Imt1YmUtYXBpc2VydmVyIiwiY3B1TGltaXQiOiIzMCIsImNwdVJlcXVlc3QiOiIyNyIsIm1lbW9yeUxpbWl0IjoiNjRHaSIsIm1lbW9yeVJlcXVlc3QiOiI2NEdpIiwiZ29tYXhwcm9jcyI6MzB9XSAg,ControlPlaneUnderlay=hcp-underlay-eastus2-cx-382,AKSHTTPCustomFeatures=OverrideControlplaneResources
 fi
 
-if [ "${USER_POOL_COUNT}" -gt 0 ]; then
-    for i in $(seq 1 ${USER_POOL_COUNT}); do
-        USER_POOL_NAME=user${i}
-        if az aks nodepool show --resource-group ${RESOURCE_GROUP} --cluster-name ${CLUSTER_NAME} --name ${USER_POOL_NAME} &>/dev/null; then
-            echo "User pool ${i} already exists."
-        else
-            echo "User pool ${i} does not exist. Creating ..."
-            az aks nodepool add \
-                --resource-group ${RESOURCE_GROUP} \
-                --cluster-name ${CLUSTER_NAME} \
-                --name ${USER_POOL_NAME} \
-                --node-vm-size ${USER_VM_SIZE} \
-                --node-count ${USER_POOL_SIZE}
-        fi
-    done
-fi
+az aks nodepool add \
+    --resource-group ${RESOURCE_GROUP} \
+    --cluster-name ${CLUSTER_NAME} \
+    --name virtual \
+    --node-vm-size Standard_D32ds_v5 \
+    --node-count 20
+
+az aks nodepool add \
+    --resource-group ${RESOURCE_GROUP} \
+    --cluster-name ${CLUSTER_NAME} \
+    --name runner \
+    --node-vm-size Standard_D32ds_v5 \
+    --node-count 20
 
 az aks get-credentials --resource-group ${RESOURCE_GROUP} \
     --name ${CLUSTER_NAME} \
