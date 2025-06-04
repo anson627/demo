0b8a3a9a89461821ab04c8ddd78061c066462732
Anson Qian <anson627@gmail.com>
2025-06-04 15:08:37 -0400

    update

--- aks-test/kwok/deploy.sh
+++ aks-test/kwok/deploy.sh
@@ -2,16 +2,8 @@
 
 set -eo pipefail
 
-NODE_COUNT=5000
 NAMESPACE_COUNT=150
 PODS_PER_NAMESPACE=1000
-
-if helm list -q | grep "^node$" > /dev/null 2>&1; then
-  echo "node release already exists, skipping..."
-else
-  helm upgrade --install node node \
-    --set "nodeCount=$NODE_COUNT"
-fi
 
 for i in $(seq 1 $NAMESPACE_COUNT); do
   namespace="kwok-$i"
