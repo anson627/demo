ba70be3b77432586eba516933f1023a99b34666b
Anson Qian <ansonqian@microsoft.com>
2025-08-17 19:23:15 -0400

    add config

--- cl2/run.sh
+++ cl2/run.sh
@@ -6,7 +6,7 @@ PROVIDER="aks"
 
 clusterloader2 --provider=$PROVIDER --v=2 \
   --enable-exec-service=False \
-  --kubeconfig=/Users/ansonqian/.kube/config \
+  --kubeconfig=$HOME/.kube/config \
   --testconfig=config.yaml \
   --testoverrides=overrides.yaml \
   --report-dir $PROVIDER-$(date +%Y%m%d-%H%M%S)
