e004265318cb2225860cfbc0c6d66bd5c46c3bc3
Anson Qian <anson627@gmail.com>
2025-08-27 19:09:34 -0400

    update

--- kwok/run.sh
+++ kwok/run.sh
@@ -4,6 +4,6 @@ for ((i = 0; i < 5000; i++)); do
   export node_name="kwok-node-$i"
   envsubst '${node_name}' < config/node.yaml | kubectl apply -f -
 
-  # export resource_slice_name="kwok-resource-slice-$i"
-  # envsubst '${node_name},${resource_slice_name}' < config/resource-slice.yaml | kubectl apply -f -
+  export resource_slice_name="kwok-resource-slice-$i"
+  envsubst '${node_name},${resource_slice_name}' < config/resource-slice.yaml | kubectl apply -f -
 done
