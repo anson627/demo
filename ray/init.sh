6e7893939ae0d1d7fed51c25a90056a39b3c5e34
Anson Qian <ansonqian@microsoft.com>
2025-08-30 13:02:39 +0000

    update

--- ray/init.sh
+++ ray/init.sh
@@ -1,16 +1,8 @@
-helm repo add kuberay https://ray-project.github.io/kuberay-helm/
-helm install kuberay-operator kuberay/kuberay-operator --version 1.4.2 -n kube-system
-
+helm upgrade --install kuberay-operator $HOME/go/src/github.com/anson627/kuberay/helm-chart/kuberay-operator -f config/values.yaml
 kubectl patch deploy kuberay-operator --type=json -p='[{"op":"add","path":"/spec/template/spec/nodeName","value":"kwok-kwok-control-plane"}]'
-
-kubectl get configmap coredns -n kube-system -o yaml > config/coredns.yaml
-sed -i '' '/ready/a\
-        rewrite name regex (.+)-head-svc\.(.+)\.svc\.cluster\.local mock-head.default.svc.cluster.local
-' config/coredns.yaml
 
 kubectl apply -f config/coredns.yaml
 kubectl patch deployment coredns -n kube-system --type=json -p='[{"op":"add","path":"/spec/template/spec/nodeName","value":"kwok-kwok-control-plane"}]'
 
-kubectl apply -f config/configmap.yaml
 kubectl apply -f config/deployment.yaml
 kubectl apply -f config/service.yaml
