e3ca4edcfe4312faa2206fdb5eff599cbe994236
Anson Qian <anson627@gmail.com>
2025-09-08 12:01:58 -0400

    update

--- aks-test/scale/setup.sh
+++ aks-test/scale/setup.sh
@@ -28,25 +28,25 @@ else
         --outbound-type managedNATGateway \
         --nat-gateway-managed-outbound-ip-count 5 \
         --custom-configuration ./custom-config.json \
-        --aks-custom-headers OverrideControlplaneResources=W3siY29udGFpbmVyTmFtZSI6Imt1YmUtYXBpc2VydmVyIiwiY3B1TGltaXQiOiIzMCIsImNwdVJlcXVlc3QiOiIyNyIsIm1lbW9yeUxpbWl0IjoiNjRHaSIsIm1lbW9yeVJlcXVlc3QiOiI2NEdpIiwiZ29tYXhwcm9jcyI6MzB9XSAg,ControlPlaneUnderlay=hcp-underlay-eastus2-cx-382,AKSHTTPCustomFeatures=OverrideControlplaneResources,AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=$IMAGE_SUB_ID,OSImageResourceGroup=$IMAGE_RG,OSImageGallery=$IMAGE_GALLERY,OSImageName=$IMAGE_NAME,OSImageVersion=$IMAGE_VERSION
+        --aks-custom-headers AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=$IMAGE_SUB_ID,OSImageResourceGroup=$IMAGE_RG,OSImageGallery=$IMAGE_GALLERY,OSImageName=$IMAGE_NAME,OSImageVersion=$IMAGE_VERSION
 fi
 
 
-for i in $(seq 1 ${USER_POOL_COUNT}); do
-    USER_POOL_NAME=user${i}
-    if az aks nodepool show --resource-group ${RESOURCE_GROUP} --cluster-name ${CLUSTER_NAME} --name ${USER_POOL_NAME} &>/dev/null; then
-        echo "User pool ${i} already exists."
-    else
-        echo "User pool ${i} does not exist. Creating ..."
-        az aks nodepool add \
-            --resource-group ${RESOURCE_GROUP} \
-            --cluster-name ${CLUSTER_NAME} \
-            --name ${USER_POOL_NAME} \
-            --node-vm-size ${USER_VM_SIZE} \
-            --node-count ${USER_POOL_SIZE} \
-            --aks-custom-headers AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=$IMAGE_SUB_ID,OSImageResourceGroup=$IMAGE_RG,OSImageGallery=$IMAGE_GALLERY,OSImageName=$IMAGE_NAME,OSImageVersion=$IMAGE_VERSION
-    fi
-done
+# for i in $(seq 1 ${USER_POOL_COUNT}); do
+#     USER_POOL_NAME=user${i}
+#     if az aks nodepool show --resource-group ${RESOURCE_GROUP} --cluster-name ${CLUSTER_NAME} --name ${USER_POOL_NAME} &>/dev/null; then
+#         echo "User pool ${i} already exists."
+#     else
+#         echo "User pool ${i} does not exist. Creating ..."
+#         az aks nodepool add \
+#             --resource-group ${RESOURCE_GROUP} \
+#             --cluster-name ${CLUSTER_NAME} \
+#             --name ${USER_POOL_NAME} \
+#             --node-vm-size ${USER_VM_SIZE} \
+#             --node-count ${USER_POOL_SIZE} \
+#             --aks-custom-headers AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=$IMAGE_SUB_ID,OSImageResourceGroup=$IMAGE_RG,OSImageGallery=$IMAGE_GALLERY,OSImageName=$IMAGE_NAME,OSImageVersion=$IMAGE_VERSION
+#     fi
+# done
 
 az aks get-credentials --resource-group ${RESOURCE_GROUP} \
     --name ${CLUSTER_NAME} \
