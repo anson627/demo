5101f0762650fd0c71ffb2e4781ec3e1f6108618
Anson Qian <anson627@gmail.com>
2025-10-22 22:55:09 -0400

    update

--- aks/scale/setup.sh
+++ aks/scale/setup.sh
@@ -15,19 +15,22 @@ fi
 if az aks show -g ${RESOURCE_GROUP} -n ${CLUSTER_NAME} &>/dev/null; then
     echo "Managed cluster already exists."
 else
+    # --custom-configuration ./custom-config.json \
     echo "Managed cluster does not exist. Creating ..."
     az aks create -l ${LOCATION} \
         -g ${RESOURCE_GROUP} \
         -n ${CLUSTER_NAME} \
         --tier standard \
-        --kubernetes-version 1.33.2 \
+        --kubernetes-version 1.33.3 \
+        --disable-disk-driver \
+        --disable-file-driver \
         --nodepool-name system \
+        --vm-set-type "VirtualMachines" \
         --node-vm-size ${SYSTEM_VM_SIZE} \
         --node-count ${SYSTEM_POOL_SIZE} \
         --network-plugin none \
         --outbound-type managedNATGateway \
         --nat-gateway-managed-outbound-ip-count 5 \
-        --custom-configuration ./custom-config.json \
         --aks-custom-headers OverrideControlplaneResources=W3siY29udGFpbmVyTmFtZSI6Imt1YmUtYXBpc2VydmVyIiwiY3B1TGltaXQiOiIzMCIsImNwdVJlcXVlc3QiOiIyNyIsIm1lbW9yeUxpbWl0IjoiNjRHaSIsIm1lbW9yeVJlcXVlc3QiOiI2NEdpIiwiZ29tYXhwcm9jcyI6MzB9XSAg,ControlPlaneUnderlay=hcp-underlay-eastus2-cx-382,AKSHTTPCustomFeatures=OverrideControlplaneResources,AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=$IMAGE_SUB_ID,OSImageResourceGroup=$IMAGE_RG,OSImageGallery=$IMAGE_GALLERY,OSImageName=$IMAGE_NAME,OSImageVersion=$IMAGE_VERSION
 fi
 
@@ -42,6 +45,7 @@ for i in $(seq 1 ${USER_POOL_COUNT}); do
             --resource-group ${RESOURCE_GROUP} \
             --cluster-name ${CLUSTER_NAME} \
             --name ${USER_POOL_NAME} \
+            --vm-set-type "VirtualMachines" \
             --node-vm-size ${USER_VM_SIZE} \
             --node-count ${USER_POOL_SIZE} \
             --aks-custom-headers AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=$IMAGE_SUB_ID,OSImageResourceGroup=$IMAGE_RG,OSImageGallery=$IMAGE_GALLERY,OSImageName=$IMAGE_NAME,OSImageVersion=$IMAGE_VERSION
