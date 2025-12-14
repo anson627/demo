94778010745cf45b702a4d99de97a414d27990d7
Anson Qian <ansonqian@microsoft.com>
2025-12-14 03:03:31 +0000

    update

--- aks/gpu/setup.sh
+++ aks/gpu/setup.sh
@@ -29,13 +29,14 @@ else
         --network-plugin azure
 fi
 
+
 if az aks nodepool show --resource-group ${RESOURCE_GROUP} --cluster-name ${CLUSTER_NAME} --name user &>/dev/null; then
     echo "User pool already exists."
 else
     echo "User pool does not exist. Creating ..."
     # --tags "TipNode.SessionId=${TIP_SESSION_ID}" \
     # --aks-custom-headers "AKSHTTPCustomFeatures=Microsoft.ContainerService/UseCustomizedOSImage,OSImageSubscriptionID=${IMAGE_SUB_ID},OSImageResourceGroup=${IMAGE_RG},OSImageGallery=${IMAGE_GALLERY},OSImageName=${IMAGE_NAME},OSImageVersion=${IMAGE_VERSION}" \
-    az aks nodepool add \
+     az aks nodepool add \
         --resource-group ${RESOURCE_GROUP} \
         --cluster-name ${CLUSTER_NAME} \
         --name user \
@@ -49,26 +50,10 @@ az aks get-credentials --resource-group ${RESOURCE_GROUP} \
     --overwrite-existing
 
 
-helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
-helm repo update
+# helm upgrade gpu-operator nvidia/gpu-operator --reuse-values --set devicePlugin.enabled=false -n gpu-operator
 
-helm install network-operator nvidia/network-operator \
-  --version v25.7.0 \
-  --create-namespace \
-  --namespace network-operator \
-  -f network-values.yaml 
-
-kubectl apply -f nfd-network-rules.yaml
-kubectl apply -f nic-cluster-policy.yaml
-
-helm install gpu-operator nvidia/gpu-operator \
-    --version=v25.10.0 \
-    --create-namespace \
-    --namespace gpu-operator \
-    -f gpu-values.yaml
-
-helm install nvidia-dra-driver-gpu nvidia/nvidia-dra-driver-gpu \
-    --version=25.8.0 \
-    --create-namespace \
-    --namespace nvidia-dra-driver-gpu \
-    -f dra-values.yaml
+# helm install dra-driver nvidia/nvidia-dra-driver-gpu \
+#     --version=25.8.0 \
+#     --create-namespace \
+#     --namespace dra-driver \
+#     -f dra/values.yaml
